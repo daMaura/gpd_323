cc85780f8ec09c36c2d70fa42d9e38c80b2080c0
Julian Maurer <julian.maurer74924@gmail.com>
2023-04-14 14:54:25 +0200

    Update Program.cs

--- gpd_323/gpd_323/Snake/Program.cs
+++ gpd_323/gpd_323/Snake/Program.cs
@@ -1,117 +1,150 @@
 ﻿
+using System;
 
-//the current length of the snake
-int snakeLength = 5;
-
-//the positions of the snakes elements
-List<(int left, int top)> SnakePos = new List<(int left, int top)>();
-
-//init random
-Random random = new Random();
-
-//spawn first food
-(int left, int top) foodPos = (random.Next(0, Console.WindowWidth), random.Next(0, Console.WindowHeight));
-Console.SetCursorPosition(foodPos.left, foodPos.top);
-Console.Write('*');
-
-//set start position
-DirectionTypes direction = DirectionTypes.Right;
-Console.SetCursorPosition(5, 5);
-
-
-//game Loop
-while (true)
+namespace Program
 {
-    //player input
-    if (Console.KeyAvailable)
-    {
-        var key = Console.ReadKey(true);
-
-
-        if (key.Key == ConsoleKey.W)
-            direction = DirectionTypes.Up;
-
-        if (key.Key == ConsoleKey.A)
-            direction = DirectionTypes.Left;
-
-        if (key.Key == ConsoleKey.S)
-            direction = DirectionTypes.Down;
-
-        if (key.Key == ConsoleKey.D)
-            direction = DirectionTypes.Right;
-    }
-
-    //move snake in a direction
-    switch (direction)
+    public static class Program
     {
 
-        case DirectionTypes.Up:
-            Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
-            break;
-        case DirectionTypes.Down:
-            Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
-            break;
-        case DirectionTypes.Left:
-            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-            break;
-        case DirectionTypes.Right:
-            Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
-            break;
-        default:
-            break;
-    }
+        //the current length of the snake
+        static int snakeLength = 100;
 
-    //check if the snake is colliding with itself
-    foreach ((int left, int top) item in SnakePos)
-    {
-        (int left, int top) pos = (Console.CursorLeft, Console.CursorTop);
+        //the positions of the snakes elements
+        static List<(int left, int top)> SnakePos = new List<(int left, int top)>();
+        static (int left, int top) SnakeHeadPos;
 
-        if (item == pos)
+        //init random
+        static Random random = new Random();
+
+        //spawn first food
+        static (int left, int top) foodPos = (0, 0);
+        public static void Main()
         {
-            Environment.Exit(0);
+            //set start position
+            DirectionTypes direction = DirectionTypes.Right;
+            Console.SetCursorPosition(5, 5);
+            SnakeHeadPos = (Console.CursorLeft, Console.CursorTop);
+
+            SpawnFood();
+
+            //game Loop
+            while (true)
+            {
+                //player input
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+
+
+                    if (key.Key == ConsoleKey.W)
+                        direction = DirectionTypes.Up;
+
+                    if (key.Key == ConsoleKey.A)
+                        direction = DirectionTypes.Left;
+
+                    if (key.Key == ConsoleKey.S)
+                        direction = DirectionTypes.Down;
+
+                    if (key.Key == ConsoleKey.D)
+                        direction = DirectionTypes.Right;
+                }
+
+                (int left, int top) newPos = (0, 0);
+
+                //move snake in a direction
+                switch (direction)
+                {
+                    case DirectionTypes.Up:
+                        newPos = (SnakeHeadPos.left, SnakeHeadPos.top - 1);
+                        break;
+                    case DirectionTypes.Down:
+                        newPos = (SnakeHeadPos.left, SnakeHeadPos.top + 1);
+                        break;
+                    case DirectionTypes.Left:
+                        newPos = (SnakeHeadPos.left - 2, SnakeHeadPos.top);
+                        break;
+                    case DirectionTypes.Right:
+                        newPos = (SnakeHeadPos.left + 2, SnakeHeadPos.top);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (newPos.left < 0)
+                    newPos.left = Console.WindowWidth - 1;
+
+                if (newPos.left > Console.WindowWidth - 1)
+                    newPos.left = 0;
+
+                if (newPos.top < 0)
+                    newPos.top = Console.WindowHeight - 1;
+
+                if (newPos.top > Console.WindowHeight - 1)
+                    newPos.top = 0;
+
+
+                SnakeHeadPos = newPos;
+
+                
+
+                //check if the snake is colliding with itself
+                foreach ((int left, int top) item in SnakePos)
+                {
+                    (int left, int top) pos = SnakeHeadPos;
+
+                    if (item == pos)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+                //check if the snake is on the position of the food
+                if (foodPos == SnakeHeadPos)
+                {
+                    //grow snake
+                    snakeLength++;
+                    SpawnFood();
+                }
+
+                //write a new character for the snake
+                Console.SetCursorPosition(SnakeHeadPos.left, SnakeHeadPos.top);
+                Console.Write('#');
+
+                //save the position of the new snake element
+                SnakePos.Add(SnakeHeadPos);
+
+                //delete the last character from the snake
+                if (SnakePos.Count > snakeLength)
+                {
+                    Console.SetCursorPosition(SnakePos[0].left, SnakePos[0].top);
+                    Console.Write(" ");
+                    SnakePos.RemoveAt(0);
+                }
+
+                Thread.Sleep(50);
+            }
+
+
+
+        }
+
+        public static void SpawnFood()
+        {
+            do
+            {
+                foodPos = (random.Next(0, Console.WindowWidth), random.Next(0, Console.WindowHeight));
+            } while ((foodPos.left % 2) == 0);
+
+            Console.SetCursorPosition(foodPos.left, foodPos.top);
+            Console.Write('*');
         }
     }
-
-    //check if the snake is on the position of the food
-    if (foodPos == (Console.CursorLeft, Console.CursorTop))
+    public enum DirectionTypes
     {
-        //grow snake
-        snakeLength++;
-
-        (int left, int top) cursorPos = (Console.CursorLeft, Console.CursorTop);
-        foodPos = (random.Next(0, Console.WindowWidth), random.Next(0, Console.WindowHeight));
-        Console.SetCursorPosition(foodPos.left, foodPos.top);
-        Console.Write('*');
-        Console.SetCursorPosition(cursorPos.left, cursorPos.top);
+        Up,
+        Down,
+        Left,
+        Right,
     }
-
-    //write a new character for the snake
-    Console.Write('#');
-    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-
-    //save the position of the new snake element
-    SnakePos.Add((Console.CursorLeft, Console.CursorTop));
-
-    //delete the last character from the snake
-    if (SnakePos.Count > snakeLength)
-    {
-        (int left, int top) curserPos = (Console.CursorLeft, Console.CursorTop);
-        Console.SetCursorPosition(SnakePos[0].left, SnakePos[0].top);
-        Console.Write(" ");
-        Console.SetCursorPosition(curserPos.left, curserPos.top);
-
-        SnakePos.RemoveAt(0);
-    }
-
-    Thread.Sleep(50);
 }
 
-
-
-public enum DirectionTypes
-{
-    Up,
-    Down,
-    Left,
-    Right,
-}
